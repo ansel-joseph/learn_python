4c55ac4fa73b7d0e0d8009fb089922a054d03a4c
Ansel Joseph <anseljoseph2509@gmail.com>
2025-06-08 18:01:21 +0530

    add: string methods

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
-course="Python \nProgramming"
-print(course)
+course="    PyThoN PRogramMing"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.find("PyT"))
+print(course.replace("P","b"))
+print("Ming" in course)
