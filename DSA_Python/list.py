f5bfb634a2f41242528371f52f8adf5ac3a6aa93
Ansel Joseph <anseljoseph2509@gmail.com>
2026-03-30 01:19:59 +0530

    add: reverse

--- DSA_Python/list.py
+++ DSA_Python/list.py
@@ -1,14 +1,8 @@
 #LISTS
 l1 = [10, 20, 30, 40, 50]
-target = 30
-for i in range(len(l1)):
-    if l1[i] == target:
-        print("Target Found at index ", i)
-        break;
-else:
-    print("Target Not Found")
-    
-    
+l2=[]
 
-        
+for i in range(len(l1)-1, -1, -1):
+    l2.append(l1[i])
 
+print(l2)
