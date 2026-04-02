b16b631ba2f2006b4ea798043a9070d30e9e03a9
Ansel Joseph <anseljoseph2509@gmail.com>
2026-04-02 23:57:10 +0530

    add: sum of numbers

--- DSA_Python/list.py
+++ DSA_Python/list.py
@@ -1,10 +1,9 @@
 #LISTS
 l1 = [100, 2, 30, 40, 5]
-first = l1[0]
-for i in l1:    
-    if first>i:
-        first=i
-print(first)
+total = 0
+for i in l1:
+    total=total+i
+print(total)
 
          
      
